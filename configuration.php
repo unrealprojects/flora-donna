67c8fccdfcc01e0ce533607a61395ff2055f03e5
Олег <office@unrealprojects.com>
2014-10-31 15:49:43 +0200

    1

--- configuration.php
+++ configuration.php
@@ -12,7 +12,7 @@ class JConfig {
 	public $debug = '0';
 	public $debug_lang = '0';
 	public $dbtype = 'mysqli';
-	public $host = 'sql.unrealprojects.com';
+	public $host = 'localhost';
 	public $user = 'root';
 	public $password = '987975';
 	public $db = 'flora-donna';
